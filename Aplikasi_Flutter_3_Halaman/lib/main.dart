5693aa9cd33ca17fbc751477c9746e187664c773
AgilDeriansyahHasan <agilderiansyahhasan@gmail.com>
2025-06-20 22:04:43 +0700

    Memindahkan Ke Dalam Folder

--- Aplikasi_Flutter_3_Halaman/lib/main.dart
+++ Aplikasi_Flutter_3_Halaman/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aplikasi Minimalis',
+      title: 'Aplikasi Flutter 3 Halaman',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.grey[100],
         textTheme: const TextTheme(
